f7dde350ad98e588c39281a441cc6e6bb4abca4e
sylv256 <contact@sylv.gay>
2026-05-04 16:10:31 -0400

    fix(adventure): take player argument in map commands

--- src/main/java/io/github/rehtea/syncope/impl/ModCommands.java
+++ src/main/java/io/github/rehtea/syncope/impl/ModCommands.java
@@ -22,12 +22,13 @@ public final class ModCommands {
 	public static void initialize() {
 		CommandRegistrationCallback.EVENT.register((dispatcher, _, _) -> {
 			dispatcher.register(Commands.literal(MOD_ID + ":music_stage").executes(context -> {
+				ServerPlayer player = context.getArgument("player", ServerPlayer.class);
 				String id = context.getArgument("music_stage", String.class).toLowerCase(Locale.ROOT);
 
 				for (MusicStage stage : MusicStage.values()) {
 					if (!stage.name().toLowerCase(Locale.ROOT).equals(id)) continue;
 
-					context.getSource().getPlayer().setAttached(ModAttachments.MUSIC_STAGE, stage);
+					player.setAttached(ModAttachments.MUSIC_STAGE, stage);
 
 					return 0;
 				}
@@ -36,12 +37,13 @@ public final class ModCommands {
 			}));
 
 			dispatcher.register(Commands.literal(MOD_ID + ":dream_layer").executes(context -> {
+				ServerPlayer player = context.getArgument("player", ServerPlayer.class);
 				String id = context.getArgument("dream_layer", String.class).toLowerCase(Locale.ROOT);
 
 				for (DreamLayer layer : DreamLayers.PATH) {
 					if (!layer.identifier().getPath().equals(id)) continue;
 
-					context.getSource().getPlayer().setAttached(ModAttachments.DREAM_LAYER, layer);
+					player.setAttached(ModAttachments.DREAM_LAYER, layer);
 
 					return 0;
 				}
